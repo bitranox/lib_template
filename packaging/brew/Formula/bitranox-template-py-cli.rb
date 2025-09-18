28e3ebd5336d5bee3123c3dda05332320dea859c
bitranox <rnowotny1966@gmail.com>
2025-09-18 19:29:55 +0200

    test: auto commit before Codecov upload

--- packaging/brew/Formula/bitranox-template-py-cli.rb
+++ packaging/brew/Formula/bitranox-template-py-cli.rb
@@ -4,7 +4,7 @@ class LibTemplate < Formula
   desc "Rich-powered logging helpers for colorful terminal output"
   homepage "https://github.com/bitranox/bitranox_template_py_cli"
   url "https://github.com/bitranox/bitranox_template_py_cli/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "<fill-me>"
+  sha256 "bb5ed0432bc212bf5250e09ad6de9f78c92489dc7e1a2c82206b95b6f376a2b1"
   license "MIT"
 
   depends_on "python@3.10"
@@ -20,8 +20,8 @@ class LibTemplate < Formula
   end
 
   resource "lib_cli_exit_tools" do
-    url "https://files.pythonhosted.org/packages/68/d6/a9d9332d5a62897a5ed16cc33ca8c8e4171559b78bd72ece6e48a5c0ffa2/lib_cli_exit_tools-1.1.0.tar.gz"
-    sha256 "cbd2278a1448b6377abf571efd6921d3a9fb8f23c4a1f74734e6690beec70d4b"
+    url "https://files.pythonhosted.org/packages/93/49/8361de4ae5e740a2c63833e7fedfacb4ef46d79e97c17e59f2f44206a648/lib_cli_exit_tools-1.1.1.tar.gz"
+    sha256 "0611b53214052cd2b9d65aa3a0724329ccb6b1b22840c30de3e88e2faf62154c"
   end
 
   def install
