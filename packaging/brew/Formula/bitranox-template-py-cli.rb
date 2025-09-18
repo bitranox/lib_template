6a8a09863b252a71cc5cd232c156fce6d1553f4d
bitranox <rnowotny1966@gmail.com>
2025-09-18 19:37:32 +0200

    test: auto commit before Codecov upload

--- packaging/brew/Formula/bitranox-template-py-cli.rb
+++ packaging/brew/Formula/bitranox-template-py-cli.rb
@@ -15,7 +15,7 @@ Yb893e1b0e686e8f0974f79475499f2e175ed064d3ab0f6c9872631a9dda502"
   end
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/60/6c/8ca2efa64cf75a977a0d7fac081354553ebe483345c734fb6b6515d96bbc/click-8.2.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/46/61/de6cd827efad202d7057d93e0fed9294b96952e188f7384832791c7b2254/click-8.3.0.tar.gz"
 Yb893e1b0e686e8f0974f79475499f2e175ed064d3ab0f6c9872631a9dda502"
   end
 
